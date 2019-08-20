5401168b0edf14bfa08c739c9aa84f5e49825eae
Grime <john.m.a.grime@gmail.com>
2019-08-20 16:21:11 -0500

    First commit

---
+++ Example.py
@@ -0,0 +1,393 @@
+# Simple example of using OpenCV descriptor-based matching to find a target image
+# in another image or webcam feed. Supports SIFT, SURF, or ORB algorithms; the
+# former two require OpenCV to be built with the optional contributions modules, as
+# they are patent encumbered. ORB is standard with OpenCV, and is free for all use.
+#
+# Author: John Grime, The University of Oklahoma.
+
+import sys, time
+import numpy as np
+import cv2 as cv
+
+#
+# Two little wrapper classes to keep things neat
+#
+
+class KeypointsAndDescriptors:
+
+    def __init__(self):
+        self.keypoints = []
+        self.descriptors = []
+
+    def DetectAndCompute(self, img, detector):
+        self.keypoints, self.descriptors = detector.detectAndCompute(img, None)
+
+
+class KNNMatcher:
+
+    def __init__(self):
+        self.all_matches = []
+        self.good_matches = []
+
+    def Match(self, kpd1, kpd2, matcher, Lowe_ratio_thresh = 0.7):
+        self.all_matches = matcher.knnMatch(kpd1.descriptors, kpd2.descriptors, k=2)
+        self.good_matches = []
+        for m in self.all_matches:
+            if (len(m)>=2) and (m[0].distance < Lowe_ratio_thresh*m[1].distance):
+                self.good_matches.append(m[0])
+
+#
+# Simple statistics class, based on algorithms of B. P. Welford
+# (via Knuth, "The Art of Computer Programming"). This algorithm
+# provides variance from a running input with very little storage
+# and is also robust to catastrophic cancellation.
+#
+
+class Stats:
+
+    def __init__(self):
+        self.Clear()
+
+    def Clear(self):
+        self.N = 0
+        self.S = 0.0
+        self.min, self.mean, self.max = 0.0, 0.0, 0.0
+
+    def Sum(self):
+        return self.mean * self.N
+
+    def Variance(self):
+        return (self.S/(self.N-1)) if (self.N>1) else 0.0
+
+    def StdDev(self):
+        return math.sqrt( self.Variance() )
+
+    def StdErr(self):
+        return math.sqrt(self.Variance()/self.N) if (self.N>1) else 0.0
+
+    def AddSample(self, x):
+        self.N += 1
+
+        if self.N == 1:
+            self.min = self.mean = self.max = x
+            self.S = 0.0
+            return
+
+        delta = x-self.mean
+        self.mean += delta/self.N
+        self.S += delta * (x-self.mean)
+
+        self.min = min(x,self.min)
+        self.max = max(x,self.max)
+
+#
+# Statistics sets referenced by string key or numerical index
+#
+
+class StatsSet:
+
+    def __init__(self):
+        self.key_to_idx = {}
+        self.stats_vec = []
+
+    def AddName(self,key):
+        if key in self.key_to_idx:
+            return self.key_to_idx[key]
+
+        s = Stats()
+        idx = len(self.stats_vec)
+        self.key_to_idx[key] = idx
+        self.stats_vec.append(s)
+
+        return idx
+
+    def AddNamedSample(self,key,val):
+        return self.key_to_idx[key] if (key in self.key_to_idx) else self.AddName(key)
+
+    def AddSample(self,idx,val):
+        self.stats_vec[idx].AddSample(val)
+        return idx
+
+    def Clear(self):
+        for i in range(0,len(self.stats_vec)):
+            self.stats_vec[i].Clear()
+
+
+
+def printUsage(progname):
+    print()
+    print("Usage : %s find=path [in=path] [with=x] [superpose=x] [min=N] [every=N]" % (progname) )
+    print()
+    print("Where:")
+    print()
+    print("  find : path to image to detect")
+    print("  in   : OPTIONAL path to image in which to search (default: 'webcam', i.e. use webcam feed)")
+    print("  with : OPTIONAL algorithm to use, one of 'SURF', 'SIFT', or 'ORB' (default: SIFT)")
+    print("  superpose : OPTIONAL path to image to superpose onto matched regional")
+    print("  min  : OPTIONAL minimum N matching features before bounding box drawn (default: 4)")
+    print("  every: OPTIONAL run processing every N frames (default: 1)")
+    print()
+    print("Notes:")
+    print()
+    print("The SURF and ORB algorithms can be accompanied with algorithm-specific data;")
+    print("  - for SURF, this is the Hessian tolerance e.g. 'with=SURF:400' (default value: 400')")
+    print("  - for ORB, this is the number of features e.g. 'with=ORB:500' (default value: 500')")
+    print()
+    print("The 'in' parameter can be decorated with a resize value for the data, e.g.: in=webcam:0.5,")
+    print("in=mypic.png:1.5. The default resize value is 1.0 (i.e., no resizing will be performed).")
+    print()
+
+    sys.exit(-1);
+
+def isValid(img):
+	return not ((img is None) or (img.all() is None))
+
+def loadImage(path, grayscale=True):
+    img = cv.imread(path, 1)
+    if isValid(img) == False:
+        print( 'Unable to open file "%s"' % (path) )
+        sys.exit( -1 )
+    return cv.cvtColor(img,cv.COLOR_BGR2GRAY) if (grayscale) else img
+
+#
+# Off we go ...
+#
+
+useGrayscale = False
+FLANN_INDEX_KDTREE, FLANN_INDEX_LSH = 1, 6
+kpd, knn = KeypointsAndDescriptors(), KNNMatcher()
+
+# Put default parameter values into map
+params = {
+    "find":      [""],
+    "in":        ["webcam"],
+    "with":      ["SIFT"],
+    "superpose": [""],
+    "min":       ["4"],
+    "every":     ["1"],
+    }
+
+#
+# Parse command line arguments
+#
+
+if len(sys.argv) < 2:
+    printUsage(sys.argv[0])
+
+for s in sys.argv[1:]:
+    toks = s.split("=")
+    if len(toks)>=2: params[toks[0]] = toks[1].split(":")
+
+print("Parameters:")
+for key in params:
+    print("  %s : %s" % (key, ' '.join(params[key])))
+
+minMatchesForBoundingBox = int(params["min"][0])
+processEvery = int(params["every"][0])
+useWebcam = (params["in"][0].lower() == "webcam")
+resize = 1.0 if len(params["in"])<2 else float(params["in"][1])
+
+#
+# Load reference image and superpose image. If latter defined, also resize
+# to match the reference image dimensions.
+#
+
+img_ref = loadImage(params["find"][0], useGrayscale)
+
+img_super = None
+if params["superpose"][0] != "":
+    img_super = loadImage(params["superpose"][0], useGrayscale)
+    w,h = img_ref.shape[0:2]
+    img_super = cv.resize( img_super, (h,w), interpolation=cv.INTER_AREA )
+
+#
+# Create detector and apporpriate matcher; SIFT, SURF, or ORB.
+#
+
+algo_info = params["with"]
+algo_name = algo_info[0].lower()
+
+if (algo_name == "sift"):
+    detector = cv.xfeatures2d.SIFT_create()
+    matcher = cv.FlannBasedMatcher({"algorithm":FLANN_INDEX_KDTREE})
+elif (algo_name == "surf"):
+    minHessian = 400 if (len(algo_info)<2) else int(algo_info[1])
+    detector = cv.xfeatures2d.SURF_create(minHessian)
+    matcher = cv.FlannBasedMatcher({"algorithm":FLANN_INDEX_KDTREE})
+elif (algo_name == "orb"):
+    # Default nFReatures is 500, but that tends not to work well.
+    nFeatures = 500 if (len(algo_info)<2) else int(algo_info[1])
+    detector = cv.ORB_create(nFeatures)
+    matcher = cv.BFMatcher(cv.NORM_HAMMING)
+    # Alternative to brute force matcher:
+    #matcher = cv.FlannBasedMatcher({"algorithm":FLANN_INDEX_LSH})
+else:
+    print("Unknown recogniser '%s'" % (algo_name))
+    sys.exit(-1)
+
+#
+# Get reference keypoints/descriptors
+#
+
+kpd_ref = KeypointsAndDescriptors()
+kpd_ref.DetectAndCompute(img_ref, detector)
+if len(kpd_ref.keypoints) < 4:
+    print("Need at least 4 keypoints (3 non-colinear) from reference image; got %d" % (len(ref_kpd.keypoints)))
+    sys.exit(-1)
+
+#
+# Create output window
+#
+
+cv.namedWindow("Good Matches",1)
+
+#
+# Start webcam feed, if specified
+#
+
+if useWebcam:
+    video_capture = cv.VideoCapture(0)
+    if video_capture.isOpened() == False: sys.exit(-1);
+
+#
+# Process data, either from input image or looping over webcam frames
+#
+
+stats = StatsSet()
+detect_idx = stats.AddName("detect")
+knn_idx = stats.AddName("knn")
+homography_idx = stats.AddName("homography")
+draw_idx = stats.AddName("draw")
+resize_idx = stats.AddName("resize")
+
+frameNo, fpsCounter, startTime = 0, 0, time.time()
+
+while True:
+
+    haveTransform = False
+    frameNo += 1
+    fpsCounter += 1
+
+    if useWebcam:
+        ret, img = video_capture.read()
+        if useGrayscale == True: img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    else:
+        img = loadImage( params["in"][0], useGrayscale )
+
+    if resize != 1.0:
+        t1 = time.time()
+        img = cv.resize( img, None, resize, resize )
+        stats.AddSample(resize_idx, time.time() - t1)
+
+    if (useWebcam == False) or (frameNo%processEvery == 0):
+        t1 = time.time()
+        kpd.DetectAndCompute( img, detector )
+        stats.AddSample(detect_idx, time.time() - t1)
+
+        #
+        # If e.g. camera is covered, we may not have any keypoint at all!
+        # Need at least 4 (with 3 non-colinear) for proper homology transform
+        # 
+
+        if len(kpd.keypoints) > 4:
+
+            t1 = time.time()
+            knn.Match( kpd_ref, kpd, matcher )
+            stats.AddSample(knn_idx, time.time() - t1)
+
+            sufficientGoodMatches = (len(knn.good_matches)>minMatchesForBoundingBox)
+
+            if (sufficientGoodMatches == True):
+                t1 = time.time()
+                srcPoints = np.float32([ kpd_ref.keypoints[m.queryIdx].pt for m in knn.good_matches ]).reshape(-1,1,2)
+                dstPoints = np.float32([ kpd.keypoints[m.trainIdx].pt for m in knn.good_matches ]).reshape(-1,1,2)
+                transform, mask = cv.findHomography( srcPoints, dstPoints, cv.RANSAC )
+                haveTransform = (type(transform) == np.ndarray)
+                stats.AddSample(homography_idx, time.time() - t1)
+
+    #
+    # Output to screen
+    #
+
+    rows1, cols1 = img_ref.shape[0:2]
+    rows2, cols2 = img.shape[0:2]
+
+    t1 = time.time()
+    if haveTransform == True:
+
+        #
+        # Superposition image onto matched region
+        #
+
+        if isValid(img_super):
+            img_tmp = cv.warpPerspective( img_super, transform, (cols2,rows2) )
+            img = cv.add( img, img_tmp )
+
+        #
+        # Bounding box around matched region
+        #
+
+        srcPoints = np.float32( [[0,0],[0,rows1-1],[cols1-1,rows1-1],[cols1-1,0]] ).reshape(-1,1,2)
+        dstPoints = cv.perspectiveTransform( srcPoints, transform )
+        img = cv.polylines(img,[np.int32(dstPoints)],True,255,3, cv.LINE_AA)
+
+        #
+        # Draw mapping of keypoints from reference image onto matches in current image
+        #
+
+        draw_params = {"matchesMask":[], "singlePointColor":None, "matchColor":None, "flags":2}
+        img_tmp = cv.drawMatches(
+            img_ref, kpd_ref.keypoints,
+            img, kpd.keypoints,
+            knn.good_matches,
+            None, **draw_params )
+    else:
+
+        #
+        # Mimic image layout of cv.drawMatches(), but without any matches.
+        # If we're not using grayscale, include the number of color channels
+        # when we create img_tmp.
+        #
+
+        dims = max(rows1,rows2), cols1+cols2
+        if useGrayscale == False:
+            dims = dims[0],dims[1],img.shape[2]
+
+        img_tmp = np.zeros(dims, dtype=img.dtype)
+        img_tmp[0:rows1,     0:cols1      ] = img_ref[0:rows1, 0:cols1]
+        img_tmp[0:rows2, cols1:cols1+cols2] =     img[0:rows2, 0:cols2]
+    cv.imshow("Good Matches", img_tmp)
+    stats.AddSample(draw_idx, time.time() - t1)
+
+    #
+    # Print some stats if needed. "potential fps" is how fast the code could run if only the image
+    # processing & display time is taken into account (i.e., ignores IO bottlenecks like reading
+    # from the camera etc)
+    #
+
+    now = time.time()
+    deltaTime = now - startTime
+    if (deltaTime>1.0):
+        out = "%5.1f fps : " % (float(fpsCounter)/deltaTime)
+        for key in stats.key_to_idx:
+            idx = stats.key_to_idx[key]
+            val = stats.stats_vec[idx].mean
+            out += "%s %.2g ms : " % (key,val/1e-3)
+        tmp = sum( [stats.stats_vec[idx].mean for idx in range(0,len(stats.key_to_idx))] )
+        out += "%d good matches in %dx%d frame (potential %g fps)" % (
+            len(knn.good_matches), img.shape[1], img.shape[0], 1.0/tmp )
+        print(out)
+
+        if haveTransform == True:
+            print( "| %+8.2f %+8.2f %+8.2f |" % (transform[0][0],transform[0][1],transform[0][2]) )
+            print( "| %+8.2f %+8.2f %+8.2f |" % (transform[1][0],transform[1][1],transform[1][2]) )
+            print( "| %+8.2f %+8.2f %+8.2f |" % (transform[2][0],transform[2][1],transform[2][2]) )
+        
+        fpsCounter, startTime = 0, now
+        stats.Clear()
+
+    if useWebcam == True:
+        if cv.waitKey(30) >= 0: break
+    else:
+        cv.waitKey()
+        break
---
+++ build_opencv_macos.sh
@@ -0,0 +1,351 @@
+#!/usr/bin/env bash
+
+#
+# This script assumes the user has admin rights. High Sierra (macOS 10.13) and later tightened
+# up OS security:
+#
+# 1. Homebrew seems to have problems modifying with the /usr/local directory. This is addressed
+# in this script by explicitly setting access rights.
+#
+# 2. The camera can't be accessed from the terminal using OpenCV in Python or C++ until you
+# accept a system prompt to grant access. Simply running a C++ executable does not seem to
+# generate a prompt; running a Python script that accesses the camera via OpenCV *does* generate
+# a prompt, after which you can also run the C++ executable without issues.
+#
+
+workdir=$(pwd)
+
+profile_file="${HOME}/.bash_profile"
+
+OpenCV_version="4.1.1"
+
+OpenCV_dir="${workdir}/OpenCV-${OpenCV_version}"
+OpenCV_source_dir="${OpenCV_dir}/opencv-${OpenCV_version}"
+OpenCV_contrib_dir="${OpenCV_dir}/opencv_contrib-${OpenCV_version}"
+
+VirtualEnvs_dir=".virtualenvs"
+
+do_homebrew_install="yes"
+do_pip_install="yes"
+do_misc_install="yes"
+do_source_install="yes"
+do_build="yes"
+do_install="yes"
+do_symlink="yes"
+
+# ---------------------------------------------------------------
+# 1. Install Homebrew ( https://brew.sh/ ) and ensure up-to-date:
+# ---------------------------------------------------------------
+
+if [[ "${do_homebrew_install}" == "yes" ]]
+then
+
+	echo ""
+	echo "#"
+	echo "# Installing Homebrew ..."
+	echo "#"
+	echo ""
+
+	# Homebrew website suggests this as a removal command ( https://docs.brew.sh/FAQ ):
+	# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
+
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	brew update
+fi
+
+
+# --------------------------------
+# 2. Install Python3 via Homebrew:
+# --------------------------------
+
+#
+# Homebrew on macOS High Sierra (10.13) and above have problems with /usr/local/
+# which breaks some post-install scripts for python; this manifests as e.g.
+# no "pip3" command (though "python3 -m pip ..." works).
+#
+# We therefore have multiple approaches:
+#
+# 1. Ensure certain required directories are created, and set access rights
+#    that allow Homebrew to manipulate them, before installing python
+#
+# 2. Install pip explicitly after python installed
+#
+# In any case, "brew install python" should result in the availability of
+# both python2 (>= 2.7.1) and python3 (>= 3.7.4). TensorFlow etc are now
+# compatible with python v3.7, so don't worry about needing v3.6 etc.
+#
+# Here, I'm using approach #1.
+#
+
+pip_install_option="1"
+
+if [[ "${do_pip_install}" == "yes" ]]
+then
+
+	echo ""
+	echo "#"
+	echo "# Installing pip3 ..."
+	echo "#"
+	echo ""
+
+	if [[ $(which brew) == "" ]]
+	then
+		echo "PROBLEM: No brew command found - did the Homebrew install work?"
+		exit
+	fi
+
+	if [[ "${pip_install_option}" == "1" ]]
+	then
+
+		sudo mkdir -p /usr/local/lib
+		sudo mkdir -p /usr/local/Frameworks
+		sudo chown -R $(whoami) $(brew --prefix)/*
+		brew install python
+
+	else
+		brew install python
+		curl -O https://bootstrap.pypa.io/get-pip.py
+		sudo python3 get-pip.py
+		sudo rm -rf get-pip.py ~/.cache/pip
+	fi
+
+fi
+
+
+# ------------------------------------------------------------
+# 3. Install misc prerequisites for building and using OpenCV:
+# ------------------------------------------------------------
+
+if [[ "${do_misc_install}" == "yes" ]]
+then
+
+	echo ""
+	echo "#"
+	echo "# Installing misc. items ..."
+	echo "#"
+	echo ""
+
+	if [[ $(which brew) == "" ]]
+	then
+		echo "PROBLEM: No brew command found - did the Homebrew install work?"
+		exit
+	fi
+
+	if [[ $(which pip3) == "" ]]
+	then
+		echo "PROBLEM: No pip3 command found - did the Python3 install work?"
+		exit
+	fi
+
+	brew install cmake
+	brew install jpeg libpng libtiff openexr
+	brew install eigen tbb
+
+	sudo pip3 install virtualenvwrapper
+
+	# Add some lines to profile file so we can use virtual Python environments:
+
+	echo ""                                                       >> ${profile_file}
+	echo "export WORKON_HOME=$HOME/${VirtualEnvs_dir}"            >> ${profile_file}
+	echo "export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3" >> ${profile_file}
+	echo "source /usr/local/bin/virtualenvwrapper.sh"             >> ${profile_file}
+
+	# Ensure new environment settings are active in the current shell:
+
+	source ${profile_file}
+
+	# Create a virtual python environment called "cv", ensure numpy is intalled in it.
+
+	mkvirtualenv cv -p python3
+
+	#
+	# Ensure numpy is available in the "cv" virtual Python environment; virtual environments
+	# don't inherit from the system environment by default. Note that recent macOS versions
+	# ship with numpy in the python2 environment, so try not to get confused ...
+	#
+
+	workon cv
+	sudo pip3 install numpy
+
+fi
+
+
+# --------------------------------
+# 4. Get OpenCV and contributions:
+# --------------------------------
+
+if [[ "${do_source_install}" == "yes" ]]
+then
+
+	source ${profile_file}
+	workon cv
+
+	echo ""
+	echo "#"
+	echo "# Installing source code for OpenCV and contributions ..."
+	echo "#"
+	echo ""
+
+	mkdir -p "${OpenCV_dir}"
+
+	cd ${OpenCV_dir}
+
+	curl -L -o "opencv-${OpenCV_version}.zip"         https://github.com/opencv/opencv/archive/${OpenCV_version}.zip
+	curl -L -o "opencv_contrib-${OpenCV_version}.zip" https://github.com/opencv/opencv_contrib/archive/${OpenCV_version}.zip
+
+	unzip "opencv-${OpenCV_version}.zip"
+	unzip "opencv_contrib-${OpenCV_version}.zip"
+
+	cd ${workdir}
+
+fi
+
+
+# ----------------------------------
+# 5. Build OpenCV and contributions:
+# ----------------------------------
+
+if [[ "${do_build}" == "yes" ]]
+then
+
+	source ${profile_file}
+	workon cv
+
+	echo ""
+	echo "#"
+	echo "# Building OpenCV and contributions ..."
+	echo "#"
+	echo ""
+
+	if [[ ! -d "${OpenCV_source_dir}" ]]
+	then
+		echo "No OpenCV source dir; expected '${OpenCV_source_dir}'"
+		exit
+	fi
+
+	if [[ ! -d "${OpenCV_contrib_dir}" ]]
+	then
+		echo "No OpenCV contrib source dir; expected '${OpenCV_contrib_dir}'"
+		exit
+	fi
+
+	cd ${OpenCV_source_dir}
+
+	if [[ ! -d build ]]
+	then
+		mkdir build
+	fi
+
+	cd build
+
+	#
+	# Get version'd python library file path.
+	#
+
+	cfg=$(python-config --configdir)
+	maj=$(python --version | awk '{split($2,t,"."); print t[1]}')
+	min=$(python --version | awk '{split($2,t,"."); print t[2]}')
+	py_lib="${cfg}/libpython${maj}.${min}.dylib"
+
+	#
+	# Get Python include directory: Skip leading "-I" of first token in results from
+	# "python-config --includes", and add a trailing slash.
+	#
+
+	tmp=$(python-config --includes | awk '{print $1}')
+	py_inc="${tmp:2}/"
+
+	#
+	# Prepare makefiles etc; assumes e.g. "workon" used to set Python evironment,
+	# and so "which python3" should return correct information. We override the
+	# default Python executable to use Python3.
+	#
+
+	cmake \
+	    -D CMAKE_BUILD_TYPE=RELEASE \
+	    -D CMAKE_INSTALL_PREFIX=/usr/local \
+	    -D OPENCV_EXTRA_MODULES_PATH="${OpenCV_contrib_dir}/modules" \
+	    -D OPENCV_ENABLE_NONFREE=ON \
+	    -D PYTHON3_LIBRARY=${py_lib} \
+	    -D PYTHON3_INCLUDE_DIR=${py_inc} \
+	    -D PYTHON3_EXECUTABLE=$(which python3) \
+	    -D PYTHON_DEFAULT_EXECUTABLE=$(which python3) \
+	    -D BUILD_opencv_python2=OFF \
+	    -D BUILD_opencv_python3=ON \
+	    -D BUILD_PERF_TESTS=OFF \
+	    -D BUILD_TESTS=OFF \
+	    ..
+
+	#
+	# Parallel make - as many processors as you feel brave enough to use!
+	#
+
+	make -j 8
+
+	cd ${workdir}
+
+fi
+
+
+# ------------------------------------
+# 6. Install OpenCV and contributions:
+# ------------------------------------
+
+if [[ "${do_install}" == "yes" ]]
+then
+
+	source ${profile_file}
+	workon cv
+
+	echo ""
+	echo "#"
+	echo "# Installing OpenCV and contributions ..."
+	echo "#"
+	echo ""
+
+	build_dir="${OpenCV_source_dir}/build"
+
+	if [[ ! -d "${build_dir}" ]]
+	then
+		echo "No OpenCV build dir; expected '${build_dir}'"
+		exit
+	fi
+
+	cd "${build_dir}"
+
+	sudo make install
+
+	cd ${workdir}
+
+	# Ensure we can find our OpenCV on the Python path! Otherwise, we may have import problems.
+	echo "export PYTHONPATH=\${PYTHONPATH}:${build_dir}/python_loader" >> ${profile_file}
+
+fi
+
+# -------------------------------------------
+# 7. Symlink so "import cv2" works in python:
+# -------------------------------------------
+
+if [[ "${do_symlink}" == "yes" ]]
+then
+
+	source ${profile_file}
+	workon cv
+
+	echo ""
+	echo "#"
+	echo "# Symlinking cv2 object ..."
+	echo "#"
+	echo ""
+
+	py_maj=$(python --version | awk '{split($2,t,"."); print t[1]}')
+	py_min=$(python --version | awk '{split($2,t,"."); print t[2]}')
+
+	src="/usr/local/lib/python${py_maj}.${py_min}/site-packages/cv2/python-{py_maj}.${py_min}/cv2.cpython-${py_maj}${py_min}m-darwin.so"
+	dst="${HOME}/${VirtualEnvs_dir}/cv/lib/python${py_maj}.${py_min}/site-packages/cv2.so"
+
+	echo "Adding symbolic link: ln -s ${src} ${dst}"
+
+	ln -s ${src} ${dst}
+
+fi
